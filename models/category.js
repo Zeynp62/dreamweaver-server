df15af24e4b21f5dfcafa2dccb4811c606aa00e8
kawtharnasser <kawtherhabib.ko@gmail.com>
2024-12-26 13:50:57 +0300

    edited the model

--- models/category.js
+++ models/category.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 const categorySchema = new Schema(
   {
-    category_id: { type: Number, required: true },
     categoryName: { type: String, required: true }
   },
   {

27392e888cc00c8a578d8494b1b85946e582d22a
kawtharnasser <kawtherhabib.ko@gmail.com>
2024-12-29 19:55:28 +0300

    save

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,5 +1,17 @@
 const router = require('express').Router()
 const UserController = require("../controllers/UserController")
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+    const extension = file.originalname.split('.').pop()
+    cb(null, `${file.fieldname}-${uniqueSuffix}.${extension}`)
+  }
+})
 
 router.get('/', UserController.getAllUsers)
 router.post('/', UserController.createUser)
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
+
 //Routs
 const UserRouter = require('./routes/UserRouter')
 const CategoryRouter = require('./routes/CategoryRouter')

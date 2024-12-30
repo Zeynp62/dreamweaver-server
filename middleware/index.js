387123f803e90bdff809df05a2897ce1db843d90
kawtharnasser <kawtherhabib.ko@gmail.com>
2024-12-30 10:15:23 +0300

    completed login

--- middleware/index.js
+++ middleware/index.js
@@ -25,10 +25,45 @@ const createToken = (payload) =>{
 }//return the token
 
 //this strip the token from the Bearer
+const stripToken = (req,res,next) =>{
+  try {
+    //get the token from the request header
+    const token = req.headers['authorization'].split(' ')[1]
+
+    if(token) {
+      res.locals.token = token
+      return next
+    }
+    res.status(401).send({status:'Error',msg:"Unauthorized"})
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({status: 'Error', msg: 'Strip Token Error!'})
+  }
+}
+
+// verify the user token
+const verifyToken = (req,res,next) =>{
+  const {token} = res.locals//get the token stored
+  
+  try {
+    let payload = jwt.verify(token,APP_SECRET)
+    
+    if(payload){
+      res.locals.payload = payload
+      return next()
+    }
+    res.status(401).send({status: "Error", msg: "Unauthorized"})
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({status: 'Error', msg: 'Verify Token Error!'})
+  }
+
+}
 
 module.exports = {
   hashPassword,
   comparePassword,
   createToken,
-
+  stripToken,
+  verifyToken
 }
